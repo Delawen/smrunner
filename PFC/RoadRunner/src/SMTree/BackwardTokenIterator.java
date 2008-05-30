17d2e9e7a66a3ebce36860fd91bf26b2c6adc7fc
delawen <delawen@d6bf89b9-4c4e-0410-a98f-b3578f9a00bd>
2008-05-30 23:59:48 +0000

    ItemIterators terminados

--- PFC/RoadRunner/src/SMTree/BackwardTokenIterator.java
+++ PFC/RoadRunner/src/SMTree/BackwardTokenIterator.java
@@ -4,12 +4,15 @@ package SMTree;
 // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
 // #[regen=yes,id=DCE.2D5B22C6-8E15-EDFF-919A-ED4D08A3DCEF]
 // </editor-fold> 
-public class BackwardTokenIterator extends BackwardItemIterator {
+public class BackwardTokenIterator<T> extends BackwardItemIterator<T> 
+{
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.93BE15DF-FD80-2FC4-0A43-46EF676EFD61]
     // </editor-fold> 
-    public BackwardTokenIterator () {
+    public BackwardTokenIterator(SMTreeNode<T> nodo) 
+    {
+        super(nodo);
     }
 
 }
--- PFC/RoadRunner/src/SMTree/WrapperIterator.java
+++ PFC/RoadRunner/src/SMTree/WrapperIterator.java
@@ -4,13 +4,65 @@ package SMTree;
 // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
 import java.util.Iterator;
 // #[regen=yes,id=DCE.566CA952-367C-1D55-D669-947718A197AB]
+import java.util.Stack;
 // </editor-fold> 
-public interface WrapperIterator<T> extends IteratorStrategy<T>
+public abstract class WrapperIterator<T> implements IteratorStrategy<T>
 {
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.F63E4E6A-100E-0957-66E1-BA3C89D7D515]
-    // </editor-fold> 
-    public abstract boolean isNext (T o);
 
+    private Stack<SMTreeNode<T>> array;
+        
+    public WrapperIterator(SMTreeNode<T> nodoInicial)
+    {
+        super();
+        inicializarArray(nodoInicial);
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.6C7D0D32-B042-86FF-2B3A-5AD6D127016A]
+    // </editor-fold> 
+    public boolean isNext (T o) 
+    {
+        SMTreeNode<T> nodo = next();
+        if(o.equals(nodo))
+            return true;
+        this.array.add(nodo);
+        return false;
+    }
+
+    abstract void inicializarArray(SMTreeNode<T> nodoInicial);
+
+    public void remove() 
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.245215D2-1215-CC60-AB9E-6019D1E6A4AD]
+    // </editor-fold> 
+    public SMTreeNode<T> next () 
+    {
+        return this.array.pop();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.19EB4119-4E9F-6A37-A53B-97B36533E2E8]
+    // </editor-fold> 
+    public boolean hasNext () 
+    {
+        return !this.array.empty();
+    }
+    
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.83C74261-AEF1-2AFD-05D3-C7582DC2B24A]
+    // </editor-fold> 
+    public boolean goTo (SMTreeNode<T> nodo) 
+    {
+        if(this.array.search(nodo) == -1)
+            return false; 
+            
+        inicializarArray(nodo);
+        return true;
+    }
 }
 
