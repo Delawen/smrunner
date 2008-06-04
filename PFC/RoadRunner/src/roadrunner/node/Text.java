d15a2872343af1f96fcf6591e24fbc255c90e5c3
delawen <delawen@d6bf89b9-4c4e-0410-a98f-b3578f9a00bd>
2008-06-04 16:01:46 +0000

    Items con constructor, equals y match

---
+++ PFC/RoadRunner/src/roadrunner/node/Text.java
@@ -0,0 +1,47 @@
+package roadrunner.node;
+
+// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// #[regen=yes,id=DCE.43392123-E85C-8D5D-0464-7640817C4CF6]
+// </editor-fold> 
+public class Text extends Item 
+{
+
+    /**
+     * Comprueba si dos Text hacen matching.
+     * 
+     * @param i
+     * @return boolean
+     */
+    @Override
+    public boolean match (Item i) 
+    {
+        //Si son dos textos iguales:
+        if(this.equals(i))
+            return true;
+        
+        //Si el item i es una variable:
+        if(i instanceof Variable)
+            return true;
+        
+        //Si no, no hacen matching:
+        return false;
+    }
+    
+    /**
+     * Comprueba si es un objeto item. Si lo es, comprueba que sea de tipo texto. 
+     * Si lo es, comprueba que tengan el mismo texto.
+     * 
+     * @param i
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object i)
+    {
+        if(i instanceof Text)
+            return ((Text)i).getContent().equals(this.getContent());
+        return false;
+    }
+
+
+}
+
