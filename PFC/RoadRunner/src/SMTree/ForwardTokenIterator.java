5e739e49527a0d799ae33b2a2ad5aa4cf68cec91
delawen <delawen@d6bf89b9-4c4e-0410-a98f-b3578f9a00bd>
2008-06-10 19:03:29 +0000

    añado lo que tenía hecho del token

--- PFC/RoadRunner/src/SMTree/ForwardTokenIterator.java
+++ PFC/RoadRunner/src/SMTree/ForwardTokenIterator.java
@@ -1,16 +1,131 @@
 package SMTree;
 
+import java.util.LinkedList;
+import java.util.List;
+import roadrunner.node.Item;
 
-// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-// #[regen=yes,id=DCE.838A5DBA-196A-2BBD-9AB7-7561B8B50D0C]
-// </editor-fold> 
-public class ForwardTokenIterator<T> extends ForwardItemIterator<T>
-{ 
-    
+
+public class ForwardTokenIterator extends ForwardItemIterator<Item>
+{
+    private SMTreeNode<Item> actual;
+     
     public ForwardTokenIterator() 
     {
         super();
     }
+    
+    /**
+     * Initializes the iterator (after a void constructor)
+     * 
+     * @param tree
+     * @return boolean
+     */
+    public boolean initialize(SMTree<Item> tree)
+    {
+        //Comprueba que era un iterador nuevo
+        if(actual != null)
+            throw new IllegalArgumentException("The iterator was tampered.");
+        
+        //Busca el primer token:
+        this.actual = tree.getRoot();
+        while(this.actual.getFirstChild() != null)
+            this.actual = this.actual.getFirstChild();
+        
+        //Guarda el árbol:
+        this.arbol = tree;
+        
+        return true;
+    }
+
+    @Override
+    public void inicializarVector() 
+    {
+        super.inicializarVector();
+    }
+
+    /**
+     * 
+     * @return if there is no posible next item
+     */
+    @Override
+    public boolean hasNext() 
+    {
+        return (!getPosibleNext().isEmpty());
+    }
+    
+    /**
+     * Returns the most probably "next" token (for the Mismatch creation)
+     * 
+     * @return Item
+     */
+    @Override
+    public Item next()
+    {
+        List<Item> lista = getPosibleNext();
+        if(lista.size() > 0)
+            return lista.get(0);
+        else
+            return null;
+    }
+    
+    /**
+     * Checks if the param is included on the list of all the possible next.
+     * 
+     * @param t
+     * @return boolean
+     */
+    @Override
+    public boolean isNext(Item t)
+    {
+        List<Item> lista = getPosibleNext();
+        int indice = lista.indexOf(t);
+        if(indice > 0)
+        {
+            goTo(lista.get(indice));
+            return true;
+        }    
+        else
+            return false;
+    }
+    
+    /**
+     * Set the pointer on the param item.
+     * Returns false if it doesn't find the item on the original tree.
+     * 
+     * @param objeto
+     * @return boolean
+     */
+    @Override
+    
+    public boolean goTo (Item objeto) 
+    {
+        if(this.arbol.getNode(objeto) != null)
+            return introducirElementos(search(objeto));
+        return false;
+    }
+    
+    
+    /**
+     * Returns all the posible nexts.
+     * 
+     * @return List<Item>
+     */
+    //TODO
+    private List<Item> getPosibleNext()
+    {
+        List<Item> lista = new LinkedList<Item>();
+        
+        
+        return lista;
+    }
+    
+    //Not used functions:
+    @Override
+    boolean introducirElementos(SMTreeNode<Item> nodoInicial) 
+    {
+        throw new NotImplementedException();
+    }
+
     
 }
 
