3fd50e0bbd14b183ae84a3754b7aae0e88f9f164
delawen <delawen@d6bf89b9-4c4e-0410-a98f-b3578f9a00bd>
2008-06-15 16:29:39 +0000

    ForwardTokenIterator funciona con next(), isNext() y hasNext()

--- PFC/RoadRunner/test/roadrunner/iterator/ForwardTokenIteratorTest.java
+++ PFC/RoadRunner/test/roadrunner/iterator/ForwardTokenIteratorTest.java
@@ -26,19 +26,26 @@ public class ForwardTokenIteratorTest {
     public ForwardTokenIteratorTest() {
     }
 
-    private SMTree<Item> arbol;
+    private static SMTree<Item> arbol;
     
     @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() 
+    public static void setUpClass() throws Exception 
     {
+              
+        /*
+         * 
+         * 
+         * 
+         *         /- primer hijo de la raiz     
+         *        /-- segundo hijo de la raiz    /- primer hijo de la lista
+         * Tuple-|----------------------- Lista-|-- segundo hijo de la lista    /- primer hijo del opcional
+         *        \-- cuarto hijo de la raiz     \-- Opcional------------------|
+         *                                                                     |
+         *                                                                   Lista
+         *                                                                    /  \
+         *                                                             Variable  segundo elemento de la segunda lista
+         * */
+        
         arbol = new SMTree<Item>();
         //Raiz: tuple
         SMTreeNode<Item> nodo = new SMTreeNode<Item>(new Tuple());
@@ -76,13 +83,23 @@ public class ForwardTokenIteratorTest {
         
         
         //Cuarto nivel: hijos de la lista, hija del opcional, hijo de la lista
-        nodo3 = new SMTreeNode<Item>(new Variable());
-        arbol.addSMTreeNode(nodo3, nodo2, Kinship.CHILD);
+        nodo2 = new SMTreeNode<Item>(new Variable());
+        arbol.addSMTreeNode(nodo2, nodo3, Kinship.CHILD);
         
-        nodo2 = new SMTreeNode<Item>(new Text("segundo hijo de la lista"));
-        arbol.addSMTreeNode(nodo2, nodo, Kinship.CHILD);
+        nodo2 = new SMTreeNode<Item>(new Text("segundo hijo de la segunda lista"));
+        arbol.addSMTreeNode(nodo2, nodo3, Kinship.CHILD);
         
         
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() 
+    {
+  
     }
 
     @After
@@ -120,10 +137,38 @@ public class ForwardTokenIteratorTest {
         assertEquals(resultado.getLast(), actual.getNext().getObject());
         
         //Nos vamos por el primer camino:
-        //it.goTo((Item)actual.getFirstChild().getObject());
-        actual = actual.getFirstChild().getNext();
+        it.goTo((Item)actual.getFirstChild().getObject());
+        actual = actual.getFirstChild();
         assertEquals(it.next(), actual.getObject());
         
+        actual = actual.getNext();
+        assertEquals(it.next(), actual.getObject());
+        
+        i = (Item) it.next();
+        assert(i instanceof Optional);
+        
+        //Comprueba que hay dos caminos y que son correctos
+        resultado = (LinkedList<Item>) it.next();
+        assert(resultado.size() == 2);
+        actual = actual.getNext();
+        assertEquals(resultado.getFirst(), actual.getFirstChild().getObject());
+        assertEquals(resultado.getLast(), actual.getParent().getNext().getObject());
+        
+        //Nos vamos por el primer camino:
+        it.goTo((Item)actual.getFirstChild().getObject());
+        actual = actual.getFirstChild();
+        assertEquals(it.next(), actual.getObject());
+        
+        //Seguimos por el opcional:
+        actual = actual.getNext();
+        i = (Item) it.next();
+        assert(i instanceof List);
+        
+        resultado = (LinkedList<Item>) it.next();
+        assert(resultado.size() == 2);
+        assertEquals(resultado.getFirst(), actual.getFirstChild().getObject());
+        actual = arbol.getRoot();
+        assertEquals(resultado.getLast(), actual.getLastChild().getObject());
         
     }
 
@@ -135,12 +180,16 @@ public class ForwardTokenIteratorTest {
     @Test
     public void hasNext() {
         System.out.println("hasNext");
-        ForwardTokenIterator instance = new ForwardTokenIterator();
-        boolean expResult = false;
-        boolean result = instance.hasNext();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        SMTreeIterator<Item> it = arbol.iterator(ForwardTokenIterator.class);
+        
+        it.next();
+        assertTrue(it.hasNext());
+        
+        it.goTo((Item)arbol.getRoot().getLastChild().getObject());
+        it.next();
+        assertFalse(it.hasNext());
+        
     }
 
     /**
@@ -149,13 +198,17 @@ public class ForwardTokenIteratorTest {
     @Test
     public void isNext() {
         System.out.println("isNext");
-        Item i = null;
-        ForwardTokenIterator instance = new ForwardTokenIterator();
-        boolean expResult = false;
-        boolean result = instance.isNext(i);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        SMTreeNode<Item> actual = arbol.getRoot().getFirstChild().getNext();
+        SMTreeIterator<Item> it = arbol.iterator(ForwardTokenIterator.class);
+        actual = actual.getNext();
+        
+        assertFalse(it.isNext(null));
+        it.goTo(actual.getObject());
+        assertFalse(it.isNext((Item)arbol.getRoot().getObject()));
+        it.goTo(actual.getObject());
+        assertTrue(it.isNext((Item)actual.getFirstChild().getObject()));
+        it.goTo(actual.getObject());
+        assertTrue(it.isNext((Item)actual.getNext().getObject()));
     }
 
 }
