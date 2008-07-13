ac96d4bf724511796edea26793cbb75da9b5d894
delawen <delawen@d6bf89b9-4c4e-0410-a98f-b3578f9a00bd>
2008-07-13 18:00:23 +0000

    Test del wrapper completito. No lo pasa.

--- PFC/RoadRunner/test/roadrunner/utils/WrapperTest.java
+++ PFC/RoadRunner/test/roadrunner/utils/WrapperTest.java
@@ -6,16 +6,17 @@
 package roadrunner.utils;
 
 import SMTree.SMTree;
-import SMTree.utils.Enclosure;
+import SMTree.SMTreeNode;
+import SMTree.utils.Kinship;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import roadrunner.iterator.EdibleIterator;
-import roadrunner.node.Item;
-import roadrunner.node.Token;
+import roadrunner.iterator.webPageForwardIterator;
+import static org.junit.Assert.*;
+import roadrunner.node.*;
 import roadrunner.operator.DirectionOperator;
 
 /**
@@ -24,13 +25,28 @@ import roadrunner.operator.DirectionOperator;
  */
 public class WrapperTest 
 {
+    
+    private static Wrapper instance;
 
     public WrapperTest() 
     {
     }
 
     @BeforeClass
-    public static void setUpClass() throws Exception {
+    public static void setUpClass() throws Exception 
+    {
+        SMTreeNode<Item> raiz = new SMTreeNode<Item>(new Tuple());
+        SMTree<Item> tree = new SMTree<Item>(raiz);
+        Item lista = new roadrunner.node.List();
+        tree.addObject(lista, raiz, Kinship.CHILD);
+        SMTreeNode<Item> listaNode = tree.getNode(lista);
+            tree.addObject(new Tag("<li>"), listaNode, Kinship.CHILD);
+                tree.addObject(new Tag("<p>"), listaNode, Kinship.CHILD);
+                    tree.addObject(new Variable(), listaNode, Kinship.CHILD);
+                tree.addObject(new Tag("</p>"), listaNode, Kinship.CHILD);
+            tree.addObject(new Tag("</li>"), listaNode, Kinship.CHILD);
+            
+       instance = new Wrapper(tree);
     }
 
     @AfterClass
@@ -51,27 +67,20 @@ public class WrapperTest
     @Test
     public void eatSquare() {
         System.out.println("eatSquare");
-        Edible e = null;
-        Item t = null;
-        DirectionOperator d = null;
-        Wrapper instance = new Wrapper();
+        Edible e = new Sample("test/roadrunner/utils/sample1.html");
+        EdibleIterator it = (EdibleIterator) e.iterator(webPageForwardIterator.class);
+        Item t = (Item) it.next();
+        DirectionOperator d = DirectionOperator.DOWNWARDS;
         Item expResult = null;
+        while(it.hasNext())
+        {
+            expResult = (Item) it.next();
+            if(expResult.getContent().equals("li") && expResult instanceof Tag && ((Tag)expResult).isCloseTag())
+                break;
+        }
+        assertNotNull(expResult);
         Item result = instance.eatSquare(e, t, d);
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getTree method, of class Wrapper.
-     */
-    @Test
-    public void getTree() {
-        System.out.println("getTree");
-        Wrapper instance = new Wrapper();
-        SMTree<Item> expResult = null;
-        SMTree<Item> result = instance.getTree();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -80,108 +89,38 @@ public class WrapperTest
     @Test
     public void eat() {
         System.out.println("eat");
-        Sample s = null;
-        DirectionOperator d = null;
-        Wrapper instance = new Wrapper();
-        Mismatch expResult = null;
+        Sample s = new Sample("test/roadrunner/utils/sample2.html");
+        DirectionOperator d = DirectionOperator.DOWNWARDS;
         Mismatch result = instance.eat(s, d);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of goTo method, of class Wrapper.
-     */
-    @Test
-    public void goTo() {
-        System.out.println("goTo");
-        Item i = null;
-        Wrapper instance = new Wrapper();
-        boolean expResult = false;
-        boolean result = instance.goTo(i);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of isWellFormed method, of class Wrapper.
-     */
-    @Test
-    public void isWellFormed() {
-        System.out.println("isWellFormed");
-        Token from = null;
-        Enclosure inclusionFrom = null;
-        Token to = null;
-        Enclosure inclusionTo = null;
-        Wrapper instance = new Wrapper();
-        boolean expResult = false;
-        boolean result = instance.isWellFormed(from, inclusionFrom, to, inclusionTo);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of substitute method, of class Wrapper.
-     */
-    @Test
-    public void substitute() {
-        System.out.println("substitute");
-        Item from = null;
-        Enclosure inclusionFrom = null;
-        Item to = null;
-        Enclosure inclusionTo = null;
-        SMTree what = null;
-        Wrapper instance = new Wrapper();
-        boolean expResult = false;
-        boolean result = instance.substitute(from, inclusionFrom, to, inclusionTo, what);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        assertNull(result);
+        d = DirectionOperator.UPWARDS;
+        result = instance.eat(s, d);
+        assertNull(result);      
+        s = new Sample("test/roadrunner/utils/sample3.html");
+        d = DirectionOperator.DOWNWARDS;
+        result = instance.eat(s, d);
+        assertNull(result);
+        d = DirectionOperator.UPWARDS;
+        result = instance.eat(s, d);
+        assertNull(result);
+        s = new Sample("test/roadrunner/utils/sample4.html");
+        d = DirectionOperator.DOWNWARDS;
+        result = instance.eat(s, d);
+        assertNotNull(result);
     }
 
     /**
      * Test of searchWellFormed method, of class Wrapper.
      */
     @Test
-    public void searchWellFormed() {
+    public void searchWellFormed() 
+    {
         System.out.println("searchWellFormed");
-        Token t = null;
-        Token from = null;
-        DirectionOperator d = null;
-        Wrapper instance = new Wrapper();
-        Token expResult = null;
+        Token from = (Token) instance.getTree().getRoot().getFirstChild().getFirstChild().getObject();
+        Token t = new Tag("</li>");
+        DirectionOperator d = DirectionOperator.DOWNWARDS;
+        Token expResult = (Token) instance.getTree().getRoot().getFirstChild().getLastChild().getObject();
         Token result = instance.searchWellFormed(t, from, d);
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
     }
-
-    /**
-     * Test of iterator method, of class Wrapper.
-     */
-    @Test
-    public void iterator() {
-        System.out.println("iterator");
-        Class iteratorClass = null;
-        Wrapper instance = new Wrapper();
-        EdibleIterator expResult = null;
-        EdibleIterator result = instance.iterator(iteratorClass);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of cloneSubWrapper method, of class Wrapper.
-     */
-    @Test
-    public void cloneSubWrapper() {
-        System.out.println("cloneSubWrapper");
-        Token firstTokenSquare = null;
-        Token lastTokenSquare = null;
-        Item parent = null;
-        Wrapper instance = new Wrapper();
-        Wrapper expResult = null;
-        Wrapper result = instance.cloneSubWrapper(firstTokenSquare, lastTokenSquare, parent);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
 }
