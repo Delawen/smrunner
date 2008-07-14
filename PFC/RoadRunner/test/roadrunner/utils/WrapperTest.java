5d8a0ff58579faafac18f40e5e4871976dc98c7e
delawen <delawen@d6bf89b9-4c4e-0410-a98f-b3578f9a00bd>
2008-07-14 18:48:26 +0000

    tokenIterators renovados y funcionando el eat

--- PFC/RoadRunner/test/roadrunner/utils/WrapperTest.java
+++ PFC/RoadRunner/test/roadrunner/utils/WrapperTest.java
@@ -29,6 +29,8 @@ public class WrapperTest
     private static Wrapper instance;
 
     private static Wrapper instance2;
+    
+    private static Wrapper instance3;
 
     public WrapperTest() 
     {
@@ -58,6 +60,24 @@ public class WrapperTest
             tree2.addObject(new Tag("</li>"), listaNode2, Kinship.CHILD);
         tree2.addObject(new DOF(), raiz2, Kinship.CHILD);
        instance2 = new Wrapper(tree2);
+       
+        SMTreeNode<Item> raiz3 = new SMTreeNode<Item>(new Tuple());
+        SMTree<Item> tree3 = new SMTree<Item>(raiz3);
+        tree3.addObject(new DOF(), raiz3, Kinship.CHILD);
+        Item lista3 = new roadrunner.node.List();
+        tree3.addObject(lista3, raiz3, Kinship.CHILD);
+        SMTreeNode<Item> listaNode3 = tree3.getNode(lista3);
+        Item optional = new roadrunner.node.Optional();
+            tree3.addObject(new Tag("<li>"), listaNode3, Kinship.CHILD);
+                tree3.addObject(optional, listaNode3, Kinship.CHILD);
+                SMTreeNode<Item> optionalNode = tree3.getNode(optional);
+                    tree3.addObject(new Tag("<p>"), optionalNode, Kinship.CHILD);
+                        tree3.addObject(new Variable(), optionalNode, Kinship.CHILD);
+                    tree3.addObject(new Tag("</p>"), optionalNode, Kinship.CHILD);
+                tree3.addObject(new Variable(), listaNode3, Kinship.CHILD);
+            tree3.addObject(new Tag("</li>"), listaNode3, Kinship.CHILD);
+        tree3.addObject(new DOF(), raiz3, Kinship.CHILD);
+       instance3 = new Wrapper(tree3);
     }
 
     @AfterClass
@@ -110,12 +130,11 @@ public class WrapperTest
         assertNull(result);      
         s = new Sample("test/roadrunner/utils/sample3.html");
         d = DirectionOperator.DOWNWARDS;
-        result = instance2.eat(s, d);
+        result = instance3.eat(s, d);
         assertNull(result);
         d = DirectionOperator.UPWARDS;
-        result = instance2.eat(s, d);
+        result = instance3.eat(s, d);
         assertNull(result);
-        s = new Sample("test/roadrunner/utils/sample4.html");
         d = DirectionOperator.DOWNWARDS;
         result = instance2.eat(s, d);
         assertNotNull(result);
